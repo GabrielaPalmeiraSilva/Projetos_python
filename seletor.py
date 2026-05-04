38c5f276488643b9d85fdc45f9613c87031ee9a4
GabrielaPalmeiraSilva <gabrielapalmeirasilva2510@gmail.com>
2026-05-04 09:50:43 -0300

    v1

---
+++ seletor.py
@@ -0,0 +1,8 @@
+#--Seletor de caracteres--#
+letra = input("Digite a letra")
+if letra == "A":
+   print("Você escolheu a Letra "A")
+elif letra == "B":
+   print("Você ecolheu a letra "B")
+else:
+   print("Essa opção não esta disponivel")
