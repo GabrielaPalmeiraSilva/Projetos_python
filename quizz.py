ca538b9bd78ebed39814f7e4116354e5f861baba
GabrielaPalmeiraSilva <gabrielapalmeirasilva2510@gmail.com>
2026-05-18 14:20:02 +0000

    v1

---
+++ quizz.py
@@ -0,0 +1,15 @@
+print("Quiz Futebol🙅🏻‍♂️")
+
+# pergunta 1
+print("1- quem ganhou a copa de 2002🧐?/n a- alemanha/n b- brasil/n c- argentina")
+r1 = input("resposta: ")
+
+if r1 == "b":
+    print("acertou✅/n")
+else:
+    print("errou era brasil😒/n")
+
+    # pergunta 2
+    print("2- Qual jogador é o maoir artilheiro da história da Liga dos Campeões da UEFA (Champions League)??/n a-
+
+
